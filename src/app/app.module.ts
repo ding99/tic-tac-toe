8c6bce17316961c1df6815467861a2036a925aed
Wei Ding <ding_web@yahoo.co.jp>
2021-07-31 19:44:04 -0400

    add game page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { GamePageComponent } from './components/game-page/game-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    GamePageComponent
   ],
   imports: [
     BrowserModule,
