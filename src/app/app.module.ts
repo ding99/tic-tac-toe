fec63d37d6f31a521eeaa92a0d749ec3ab70d72f
Wei Ding <ding_web@yahoo.co.jp>
2021-07-31 19:56:39 -0400

    four buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { GamePageComponent } from './components/game-page/game-page.component';
+import { MarkComponent } from './components/buttons/mark/mark.component';
+import { LevelComponent } from './components/buttons/level/level.component';
+import { UserComponent } from './components/buttons/user/user.component';
+import { ResetComponent } from './components/buttons/reset/reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    GamePageComponent
+    GamePageComponent,
+    MarkComponent,
+    LevelComponent,
+    UserComponent,
+    ResetComponent
   ],
   imports: [
     BrowserModule,
